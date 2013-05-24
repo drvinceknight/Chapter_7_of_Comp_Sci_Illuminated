72cf48239890caa4f35246226999ed6372093827
drvinceknight <vincent.knight@gmail.com>
2013-05-24 14:02:26 +0100

    Added comments to bubble sort

--- bubble_sort.py
+++ bubble_sort.py
@@ -5,6 +5,7 @@ Script for bubble sort
 from __future__ import division
 import random
 
+# Some data
 data = [
     "ant",
     "cat",
@@ -19,23 +20,31 @@ data = [
     "snake"
 ]
 
+# Shuffling the data
 random.shuffle(data)
+
+# Printing the data to sort to screen
 print "Sorting the following list:"
 for e in data:
     print "\t", e
 
+# Initialisation
 firstUnsorted = 0
 swap = True
+
+# The algorithm
 while firstUnsorted < (len(data) - 1) and swap:
     swap = False
     index = len(data) - 1
     while index > firstUnsorted:
         if data[index] < data[index - 1]:
+            # Swapping two elements 'bubbling up'
             data[index], data[index - 1] = data[index - 1], data[index]
             swap = True
         index -= 1
     firstUnsorted += 1
 
+# Printing the results to screen
 print "Here is the sorted list:"
 for e in data:
     print "\t", e
