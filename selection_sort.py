4313d52ea30681ca8e2c98bc59d1ae8137425bb5
drvinceknight <vincent.knight@gmail.com>
2013-05-24 14:01:02 +0100

    Added comment to selection sort

--- selection_sort.py
+++ selection_sort.py
@@ -5,6 +5,7 @@ Script for selection sort
 from __future__ import division
 import random
 
+# Some data
 data = [
     "ant",
     "cat",
@@ -19,12 +20,18 @@ data = [
     "snake"
 ]
 
+# Shuffling the data so that we have something to sort:
 random.shuffle(data)
+
+# Printing the data to screen
 print "Sorting the following list:"
 for e in data:
     print "\t", e
 
+# Initialisation
 firstUnsorted = 0
+
+# The algorithm
 while firstUnsorted < len(data) - 1:
     # Find smallest unsorted item
     indexOfSmallest = firstUnsorted
@@ -37,6 +44,7 @@ while firstUnsorted < len(data) - 1:
     data[firstUnsorted], data[indexOfSmallest] = data[indexOfSmallest], data[firstUnsorted]
     firstUnsorted += 1
 
+# Printing sorted data to screen
 print "Here is the sorted list:"
 for e in data:
     print "\t", e
