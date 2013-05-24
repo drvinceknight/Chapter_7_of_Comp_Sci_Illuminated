95b84ee4b15e4bdb07696c39dfb18acd289421dd
drvinceknight <vincent.knight@gmail.com>
2013-05-24 14:18:08 +0100

    Comments for recursive binary search

--- recursive_binary_search_for_sorted_array.py
+++ recursive_binary_search_for_sorted_array.py
@@ -4,6 +4,7 @@ Script for recursive binary search in a sorted array
 """
 from __future__ import division
 
+# Some data
 data = [
     "ant",
     "cat",
@@ -18,15 +19,19 @@ data = [
     "snake"
 ]
 
+# Making sure the data is sorted so that binary search will work
 data.sort()
 
+# The element we are searching for
 item = "horse"
 
+# Some initialisation
 first = 0
 last = len(data) - 1
 found = False
 
 
+# A function for binary search (which is needed for recursion)
 def binary_search(first, last):
     if first > last:
         return False
@@ -38,6 +43,5 @@ def binary_search(first, last):
     else:
         return binary_search(middle + 1, last)
 
-
+# Print the binary search
 print binary_search(first, last)
-print data.index(item)
