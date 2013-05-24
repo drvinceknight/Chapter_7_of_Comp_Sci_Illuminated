60305f3d9ef1be7c975bda729377a0e387e4b05b
drvinceknight <vincent.knight@gmail.com>
2013-05-24 09:30:27 +0100

    still working on quicksort

--- quick_sort.py
+++ quick_sort.py
@@ -25,36 +25,36 @@ for e in data:
     print "\t", e
 
 
+def split(splitVal, first, last):
+    left = first + 1
+    right = last
+    while left <= right:
+        while left <= right and data[left] <= splitVal:
+            left += 1
+        while left <= right and data[right] >= splitVal:
+            right -= 1
+        if left < right:
+            data[left], data[right] = data[right], data[left]
+    splitPoint = right
+    return splitPoint
+
+
 def quicksort(first, last):
     if first < last:
         splitVal = data[first]
         splitPoint = split(splitVal, first, last)
         quicksort(first, splitPoint - 1)
+        #print "---\n", data
         quicksort(splitPoint + 1, last)
+        #print "***\n", data
 
-
-def split(splitVal, first, last):
-    left = first
-    right = last
-    while left <= right:
-        while data[left] <= splitVal and left <= right:
-            print data[left]
-            left += 1
-        while data[right] >= splitVal and left <= right:
-            right -= 1
-        if left < right:
-            print data[left], data[right]
-            data[left], data[right] = data[right], data[left]
-        print data
-    splitPoint = right
-    return splitPoint
 
 first = 0
 last = len(data) - 1
 
 
-#quicksort(first, last)
-split(data[0], first, last)
+quicksort(first, last)
+#print "Split value is:", split(data[0], first, last)
 print "Here is the sorted list:"
 for e in data:
     print "\t", e
